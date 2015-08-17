d24ee9111397b480ee83023d2fb9aebc1eb3422d
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-17 19:58:09 -0400

    Loop over mutable vector

--- src/simplesorts.rs
+++ src/simplesorts.rs
@@ -1,8 +1,11 @@
 
-use std::fmt::Debug;
+// use std::fmt::Debug;
 
-pub fn insertion_sort<T: Debug>(array: &[T]) /*-> &[T]*/ {
-    for elem in array {
-        println!("    simplesorts::insertion_sort():  {:?}", elem);
+// pub fn insertion_sort<T: Debug>(array: &mut Vec<T>) {
+pub fn insertion_sort(array: &mut Vec<f64>) {
+    let N = array.len();
+    println!("  simplesorts::insertion_sort():  N = {:?}", N);
+    for (i,elem) in array.iter().enumerate() {
+        println!("    simplesorts::insertion_sort():  i={}   {:?}", i, elem);
     }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -3,8 +3,8 @@ extern crate sorting;
 #[test]
 fn it_works() {
     // let array: Vec<_> = vec![5, 4, 7, 2, 4, 9];
-    let array: Vec<_> = vec![5.2, 4.3, 7.4, 2.3, 4.1, 9.0];
+    let mut array: Vec<_> = vec![5.2, 4.3, 7.4, 2.3, 4.1, 9.0];
     println!("it_works()...");
-    sorting::simplesorts::insertion_sort(&array);
+    sorting::simplesorts::insertion_sort(&mut array);
     println!("it_works() done!");
 }
