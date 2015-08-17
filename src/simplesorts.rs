3fca2c381faea03f627c99c2138c072955ee9cb8
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-17 19:59:50 -0400

    Cleanup

--- src/simplesorts.rs
+++ src/simplesorts.rs
@@ -3,9 +3,12 @@
 
 // pub fn insertion_sort<T: Debug>(array: &mut Vec<T>) {
 pub fn insertion_sort(array: &mut Vec<f64>) {
-    let N = array.len();
-    println!("  simplesorts::insertion_sort():  N = {:?}", N);
-    for (i,elem) in array.iter().enumerate() {
-        println!("    simplesorts::insertion_sort():  i={}   {:?}", i, elem);
+    let n = array.len();
+
+    if n > 1 {
+        println!("  simplesorts::insertion_sort():  N = {:?}", n);
+        for (i,elem) in array.iter().enumerate() {
+            println!("    simplesorts::insertion_sort():  i={}   {:?}", i, elem);
+        }
     }
 }
