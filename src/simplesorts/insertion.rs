3f0935dcbb9f693d6872611f96ec89b6cd62ac35
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-18 13:28:35 -0400

    Insertion sort is now stable.

--- src/simplesorts/insertion.rs
+++ src/simplesorts/insertion.rs
@@ -31,11 +31,17 @@ use std::fmt::Debug;
 ///
 /// # Notes
 ///
-/// The implementation is 'unstable' as it does not preserve the relative order of items with
-/// equal values. It could be made stable though if required.
+/// The implementation is 'stable' as it does preserve the relative order of items with
+/// equal values.
 ///
 /// The type T of the vector elements to sort _must_ implement the `PartialOrd` trait so the
 /// compiler knows how to compare the elements and sort them.
+///
+/// # Optimizations
+///
+/// Every element of the vector to sort are compared to the first and last element of the sorted
+/// vector. This prevents the worst case scenario to happen. The implementation still keeps an
+/// O(N^2) scaling though.
 ///
 /// # Examples
 ///
@@ -71,23 +77,22 @@ pub fn sort<T: Debug + PartialOrd>(input: &Vec<T>) -> Vec<usize> {
                 // If the element of the loop is larger or equal to the last sorted element, simply
                 // append its index to the sorted index list.
                 output_index.push(i);
+            } else if *elem < input[*output_index.first().unwrap()] {
+                // If the element of the loop is smaller than the first sorted element, simply put
+                // it at the beginning of the sorted index list.
+                output_index.insert(0, i);
             } else {
-                println!("    2. elem ?< input[output_index[0]]");
-                println!("           {:?} ?< {:?}", elem, input[output_index[0]]);
-                // The next element of the input vector is smaller than the last stored on.
-                // Let's find where it should go by looping back on the stored indices.
-                // NOTE: we must use a loop on indices as to prevent problems with the borrow-checker.
-                if *elem < input[output_index[0]] {
-                    println!("        2a. Yes: --> putting i in front");
-                    output_index.insert(0, i);
-                } else {
-                    println!("        2b. No: --> Looping back");
-                    for j in (0..i).rev() {
-                        println!("            j={:?}   elem={:?}   input[output_index[j]]={:?}", j, elem, input[output_index[j]]);
-                        if *elem > input[output_index[j]] {
-                            output_index.insert(j+1, i);
-                            break;
-                        }
+                // If the element of the loop is smaller than the last sorted element, we loop
+                // over (backward) all sorted indices and compare the values to identify where
+                // to place the loop element.
+                // NOTE: We must use a loop on indices as to prevent problems with the
+                //       rust borrow-checker.
+
+                // Naively start at the second element of the output_index.
+                for j in (1..i) {
+                    if *elem < input[output_index[j]] {
+                        output_index.insert(j, i);
+                        break;
                     }
                 }
             }
