1f593808936b8ad5b6452e6e554640960a1392ec
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-18 08:57:08 -0400

    Insertion sort now works. Could be optimized.

--- src/simplesorts.rs
+++ src/simplesorts.rs
@@ -1,18 +1,82 @@
 
 use std::fmt::Debug;
 
-pub fn insertion_sort<T: Clone + Debug>(input: &[T]) -> &[T] {
+pub fn insertion_sort<T: Debug + PartialOrd>(input: &Vec<T>) -> Vec<usize> /*&[T]*/ {
     let n = input.len();
 
-    let output = input.clone();
+    // let mut output_index: Vec<usize> = vec![0];
+    let mut output_index: Vec<usize> = Vec::with_capacity(n);
 
-    println!("  simplesorts::insertion_sort():  n = {:?}", n);
-    println!("  simplesorts::insertion_sort():  input:  {:?}", input);
-    println!("  simplesorts::insertion_sort():  output: {:?}", output);
+    // Place the first element's index (0) as the first element of the output_index vector.
+    output_index.push(0);
 
-    for (i,elem) in input.iter().enumerate() {
-        println!("    simplesorts::insertion_sort():  i={}   {:?}", i, elem);
+    println!("n = {:?}", n);
+    println!("input:        {:?}", input);
+    println!("output_index: {:?}", output_index);
+    // println!("  output: {:?}", input[output[0]]);
+
+    if n > 1 {
+        // Loop over input vector, skipping the first element as it's already inserted as the first
+        // element of output_index.
+        for (i_start_at_0,elem) in input.iter().skip(1).enumerate() {
+            let i = i_start_at_0 + 1;
+            println!("  i={:?} Finding where to put element {:?} (i={:?}) in output_index: {:?}", i, elem, i, output_index);
+            // println!("    0. input[i={}] == elem={:?}", i, elem);
+            // println!("    0. output_index: {:?}", output_index);
+
+            // let x: () = *elem;
+
+            // println!("       *output_index.last().unwrap() = {:?}", *output_index.last().unwrap());
+            // println!("       input[*output_index.last().unwrap()] = {:?}", input[*output_index.last().unwrap()]);
+
+            // Verify next element if it is smaller
+            // If next element is larger than the last stored index, just place that element at
+            // the end.
+            if *elem >= input[*output_index.last().unwrap()] {
+                // Next element "elem" is already larger than the previous stored index.
+                // Just use its index.
+                output_index.push(i);
+
+                println!("    1. Pushing element '{:?}' at the end.", elem);
+            } else {
+                println!("    2. elem ?< input[output_index[0]]");
+                println!("           {:?} ?< {:?}", elem, input[output_index[0]]);
+                // The next element of the input vector is smaller than the last stored on.
+                // Let's find where it should go by looping back on the stored indices.
+                // NOTE: we must use a loop on indices as to prevent problems with the borrow-checker.
+                if *elem < input[output_index[0]] {
+                    println!("        2a. Yes: --> putting i in front");
+                    output_index.insert(0, i);
+                } else {
+                    println!("        2b. No: --> Looping back");
+                    for j in (0..i).rev() {
+                        println!("            j={:?}   elem={:?}   input[output_index[j]]={:?}", j, elem, input[output_index[j]]);
+                        if *elem > input[output_index[j]] {
+                            output_index.insert(j+1, i);
+                            break;
+                        }
+                    }
+                }
+                println!("        3. output_index: {:?}", output_index);
+            }
+
+            print!("           output: [");
+            for j in output_index.iter() {
+                    print!("{:?}, ", input[*j]);
+            }
+            println!("]");
+        }
     }
 
-    return output;
+    println!("END: output_index: {:?}", output_index);
+    print!("END: output: [");
+    for j in output_index.iter() {
+            print!("{:?}, ", input[*j]);
+    }
+    println!("]");
+
+
+    assert_eq!(output_index.len(), n);
+
+    return output_index;
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -2,9 +2,10 @@ extern crate sorting;
 
 #[test]
 fn it_works() {
-    // let array: Vec<_> = vec![5, 4, 7, 2, 4, 9];
-    let mut array: Vec<_> = vec![5.2, 4.3, 7.4, 2.3, 4.1, 9.0];
+    // let array: Vec<_> = vec![10, 11, 13, 14, 15, 12];
+    let array: Vec<_> = vec![15, 14, 17, 12, 14, 19];
+    // let array: Vec<_> = vec![5.2, 4.3, 7.4, 2.3, 4.1, 9.0];
     println!("it_works()...");
-    sorting::simplesorts::insertion_sort(&mut array);
+    sorting::simplesorts::insertion_sort(&array);
     println!("it_works() done!");
 }
