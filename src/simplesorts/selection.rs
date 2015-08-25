e922848ac33558aa3b41cd5e0bad698059f0ec70
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-25 14:51:33 -0400

    Bug fix: The previous sorting was taking the last element of "int_slice" smaller than "array[i]".

--- src/simplesorts/selection.rs
+++ src/simplesorts/selection.rs
@@ -60,11 +60,20 @@ pub fn sort<T: Ord + Debug>(array: &mut Vec<T>) {
                 // k now holds the index (of "array") of the minimum element of "int_slice", or the
                 // value "i" of the current element of "array" (which should be the less than
                 // all elements of "int_slice").
-                let k = int_slice.iter().enumerate().fold(i, |j, pair| {
-                    if array[i] > *pair.1 { i + 1 + pair.0 } else { j }
+                // "k" is a tuple containing the smallest element of "int_slice" and its index.
+                //     k.0: Index of "int_slice" of its smallest value.
+                //     k.1: Smallest value of "int_slice".
+                let k = int_slice.iter().enumerate().fold(
+                    (0, int_slice.first().unwrap()), |acc, item| {
+                        if acc.1 > item.1 { item } else { acc }
                 });
 
-                k
+                // Return from the block the smallest element between the one at index "i" of
+                // the "array" and the smallest one from the "int_slice" array.
+                // NOTE: Since "k" worked on the subarray "int_slice", it's first element is the
+                //       i^th element of "array". If "k" is actually smaller than "array[i]", we
+                //       need to adapt the index as stored in "k.0".
+                if k.1 < &array[i] { i + k.0 + 1 } else { i }
             };
             println!("            k: {:?}   array[i]={:?}   array[j]={:?}", j, array[i], array[j]);
 
