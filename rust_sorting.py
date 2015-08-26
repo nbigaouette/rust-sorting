5c958d5a5c862e06058bcfa44bb4e84690f036ea
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-26 10:07:44 -0400

    New python module to load rust library and expose functions.

---
+++ rust_sorting.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import ctypes
+import os
+import glob
+
+import numpy as np
+
+
+# Load the Rust library when loading this module
+
+target = "debug"
+# target = "release"
+
+libpath = os.path.join("target", target, "libsorting.*")
+libfile = glob.glob(libpath)[0]
+rustlib = ctypes.CDLL(libfile)
+
+def quicksort(array):
+    rustlib.rust_quicksort(ctypes.c_void_p(array.ctypes.data), len(array))
