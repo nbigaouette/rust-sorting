433734ef1344222d277a830bd8270d8d212d2964
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-26 14:32:32 -0400

    Cleanup: Call at end only.

--- rust_sorting.py
+++ rust_sorting.py
@@ -56,27 +56,29 @@ def quicksort(array):
     n   = len(array)
 
     if array.dtype == np.int8:
-        rustlib.ffi_quicksort_i8(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_i8
     elif array.dtype == np.int16:
-        rustlib.ffi_quicksort_i16(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_i16
     elif array.dtype == np.int32:
-        rustlib.ffi_quicksort_i32(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_i32
     elif array.dtype == np.int64:
-        rustlib.ffi_quicksort_i64(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_i64
 
     elif array.dtype == np.uint8:
-        rustlib.ffi_quicksort_u8(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_u8
     elif array.dtype == np.uint16:
-        rustlib.ffi_quicksort_u16(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_u16
     elif array.dtype == np.uint32:
-        rustlib.ffi_quicksort_u32(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_u32
     elif array.dtype == np.uint64:
-        rustlib.ffi_quicksort_u64(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_u64
 
     elif array.dtype == np.float32:
-        rustlib.ffi_quicksort_f32(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_f32
     elif array.dtype == np.float64:
-        rustlib.ffi_quicksort_f64(ptr, n)
+        rust_sort = rustlib.ffi_quicksort_f64
 
     else:
          raise NotImplementedError
+
+    rust_sort(ptr, n)
