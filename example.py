1841cba63a705aa40b121c3d70092d69fcb8adb9
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-26 12:46:37 -0400

    Cleanup

--- example.py
+++ example.py
@@ -7,19 +7,6 @@ import rust_sorting as rs
 N = 5
 max_val = 10.0
 
-# dtype = np.int8
-# dtype = np.int16
-# dtype = np.int32
-# dtype = np.int64
-
-# dtype = np.uint8
-# dtype = np.uint16
-# dtype = np.uint32
-# dtype = np.uint64
-
-# dtype = np.float32
-# dtype = np.float64
-
 dtypes = [np.int8,  np.int16,  np.int32,  np.int64,
           np.uint8, np.uint16, np.uint32, np.uint64,
           np.float32, np.float64]
