223d17aae5d4b5dfdc9df073e26401399a177547
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-26 14:27:49 -0400

    Run benchmark over different sorting algorithms.

--- benchmark.py
+++ benchmark.py
@@ -11,38 +11,42 @@ max_val = 10.0
 
 dtype = np.int32
 
-repeat = 10
+repeat = 4
 Nb_power_of_two = 20
 Ns = 2**np.arange(0, Nb_power_of_two)
 
-timing = np.zeros((Nb_power_of_two, repeat), dtype=np.float64)
+fct_ptrs = [rs.quicksort, rs.sort]
+
+timing = np.zeros((len(fct_ptrs), Nb_power_of_two, repeat), dtype=np.float64)
 
 print("Ns:", Ns)
 
 t1 = time.clock()
-for Ni, N in enumerate(Ns):
-    print("Ni:", Ni, "  N:", N)
-    for r in range(0, repeat):
-        array = np.array(max_val*np.random.rand(N), dtype=dtype)
-        t0 = t1
-        rs.quicksort(array)
-        t1 = time.clock()
-        timing[Ni, r] = t1 - t0
+for fi, f in enumerate(fct_ptrs):
+    print(f)
+    for Ni, N in enumerate(Ns):
+        print("Ni:", Ni, "  N:", N)
+        for r in range(0, repeat):
+            array = np.array(max_val*np.random.rand(N), dtype=dtype)
+            t0 = t1
+            f(array)
+            t1 = time.clock()
+            timing[fi, Ni, r] = t1 - t0
 
-print("timing:", timing)
+# print("timing:", timing)
 
-mean = np.mean(timing, axis=1)
-std  = np.std(timing, axis=1)
+mean = np.mean(timing, axis=2)
+std  = np.std(timing, axis=2)
 
-print("mean:", mean)
-print("std:", std)
-
-assert(len(mean) == Nb_power_of_two)
-assert(len(std)  == Nb_power_of_two)
+for fi, f in enumerate(fct_ptrs):
+    assert(len(mean[fi,:]) == Nb_power_of_two)
+    assert(len(std[fi,:])  == Nb_power_of_two)
 
 fig = on_key.figure()
 ax  = fig.add_subplot(1,1,1)
-ax.errorbar(Ns, mean, yerr=std, label="Quicksort")
+# ax.errorbar(Ns, mean, yerr=std, label="Quicksort")
+for fi, f in enumerate(fct_ptrs):
+    ax.errorbar(Ns, mean[fi,:], yerr=std[fi,:], label=f)
 ax.grid(True)
 ax.legend(loc='best')
 ax.set_xlabel('N')
