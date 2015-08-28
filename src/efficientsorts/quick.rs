29767ff99cd46d95751153100b5c5dbf18089e5a
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-28 15:17:53 -0400

    Optimization: Use median of first, middle and last element of vector as pivot.
    This should prevent worst case scenario of O(N^2) when vector is already sorted.

--- src/efficientsorts/quick.rs
+++ src/efficientsorts/quick.rs
@@ -12,15 +12,19 @@ use std::slice;
 ///
 /// # Details
 ///
-///
+/// Quicksort is the defacto standard of sorting algorithms. It recursively sorts a vector in place
+/// by finding a pivot and by re-arranging the vector so that all smaller elements are on the
+/// left and all larger elements are on the right of that pivot.
 ///
 /// # Scaling
 ///
-///
+/// It's average case complexity is O(N log N). To prevent the worst case complexity of O(N^2) when
+/// the vector is already sorted, the median of the first, middle and last elements is taken as the
+/// pivot ("median-of-three").
 ///
 /// # Optimizations
 ///
-/// None
+/// Use median as pivot.
 ///
 /// # Notes
 ///
@@ -45,8 +49,32 @@ pub fn sort<T: PartialOrd>(array: &mut [T]) {
             array.swap(0, 1);
         }
     } else {
-        // Use the first element as the pivot, and loop over the rest of the vector.
-        let mut pivot = 0;
+        // Choose the pivot element: Select the median between the first, middle and last element.
+        let mut pivot = {
+            let mut piv_choices: Vec<usize> = vec![0, n/2, n-1];
+            // Find maximum value between the first, middle and last element.
+            let mut max_val_i: usize = 0;
+            for i in 1..3 {
+                if array[piv_choices[max_val_i]] < array[piv_choices[i]] {
+                    max_val_i = i;
+                }
+            }
+            // Remove that index from the choice of pivot.
+            piv_choices.remove(max_val_i);
+            // The pivot will be the maximum of the remaining two (the median of initial
+            // piv_choices vector).
+            assert_eq!(piv_choices.len(), 2);
+            if array[piv_choices[0]] > array[piv_choices[1]] {
+                piv_choices[0]
+            } else {
+                piv_choices[1]
+            }
+        };
+
+        // Place pivot at i=0 and loop over the remaining of the vector.
+        array.swap(pivot, 0);
+        pivot = 0;
+
         for i in 1..n {
             // The loop element is smaller than the pivot. Shuffle things around to place that
             // element before the pivot.
