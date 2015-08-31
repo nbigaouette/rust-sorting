6f68a2c467108bf8bfeba32d91738a385f8bd7c5
Nicolas Bigaouette <nbigaouette@gmail.com>
2015-08-31 09:48:00 -0400

    Copy benchmark figure to documentation folder.

--- publish_to_gh-pages.sh
+++ publish_to_gh-pages.sh
@@ -5,6 +5,10 @@
 
 cargo doc || exit 1
 
+# Copy benchmark figure too.
+mkdir -p target/doc/sorting/figures
+cp figures/benchmark.png target/doc/sorting/figures/
+
 ghp-import -n target/doc || exit 1
 
 git push --force origin gh-pages || exit 1
